43846d09e8930070d2104f2389f021c45ecab206
Flash Millenium <flash.millenium@gmail.com>
2016-07-21 16:50:23 +0300

    Add java:exec plugin in maven first task and test write

--- src/main/java/ExerciseOne.java
+++ src/main/java/ExerciseOne.java
@@ -1,3 +1,19 @@
 
 public class ExerciseOne {
+
+
+    /*
+      Write a program that reads an integer and prints it in binary, octal, and hexadecimal.
+      Print the reciprocal as a hexadecimal floating-point number.
+     */
+    public static void printInt(int i){
+        System.out.printf("%s %s %s %8.4A",
+                Integer.toString(i,2),
+                Integer.toString(i,8),
+                Integer.toString(i,16), 1.0 /i);
+    }
+
+    public static void main(String [] args){
+        printInt(10);
+    }
 }
--- src/test/java/ExerciseOneTest.java
+++ src/test/java/ExerciseOneTest.java
@@ -1,6 +1,33 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.io.SyncFailedException;
+
 import static org.junit.Assert.*;
 
 
 public class ExerciseOneTest {
+
+    public final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    @Before
+    public void setUpStreams(){
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void cleanUpStreams(){
+        System.setOut(null);
+    }
+
+    @Test
+    public void printInt() throws Exception {
+        ExerciseOne.printInt(10);
+        assertEquals("1010 12 a 0X1.999AP-4",outContent.toString());
+    }
+
 
 }
