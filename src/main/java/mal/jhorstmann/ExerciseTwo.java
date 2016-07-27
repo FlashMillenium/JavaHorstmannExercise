875bdb7ad63bbbf639f265f30be8b9cbb79ed06b
Flash Millenium <flash.millenium@gmail.com>
2016-07-27 19:58:01 +0300

    add task from Chapter 2.

---
+++ src/main/java/mal/jhorstmann/ExerciseTwo.java
@@ -0,0 +1,34 @@
+package mal.jhorstmann;
+
+public class ExerciseTwo {
+
+    /*
+    Change the calendar printing program so it starts the week
+    on Sunday. Also make it print a newline at the end(but only one)
+     */
+
+    /*
+    Implement an immutable class Point that describes a point in the plane.
+    Provide a constructor to set it to a specific point, a no-arg constructor
+    to set it to the origin, and methods getX, getY, translate, and scale.
+    The translate method moves the point by a given amount in x- and y- direction.
+    The scale method scales both coordinates by a given factor. Implement these methods
+    so that they return new points with the results. For example,
+    Point p = new Point(3,4).translate(1,3).scale(0.5);
+    shoud set p to a point with coordinates(2, 3.5).
+     */
+
+    /* add javadoc comments to the Point class */
+
+    /* implement class Queue, an unbounded queue of strings. Provide methods add,
+    adding at the tail, and remove, removing at the head of queue.
+    Store elements as a linked list of nodes.
+    Make Node a nested class. Shoud it be static or not?
+     */
+
+    /* provide an iterator - an object that yield the elements of the queue
+    in turn - for the queue of the preceding class. Make Iterator a nested class with method next
+    and hasNext. Provide a method iterator() of the Queue class that yields a Queue.Iterator.
+    Should Iterator be static or not?
+     */
+}
